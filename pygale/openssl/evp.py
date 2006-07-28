7da5508b2909fcd11d4b16adb21917e60efaeda8
john.reese <john.reese@58f90dae-9319-0410-9eff-19c08d57e0ec>
2006-07-28 06:21:07 +0000

    - OK, that sucked.  Missed one file, checked in another before its tests   passed.

--- pygale/openssl/evp.py
+++ pygale/openssl/evp.py
@@ -1,53 +1,7 @@
-"""
-The mysterious evp module.
-
-Test by jtr, adapted from tlau's pygale/tests/test.py.
-
->>> import os
->>> td= lambda x: os.path.join(os.environ['PYGALE_TEST_DIR'], x)
-
->>> key_file= open(td('private/selma@test.yammer.net.gpri'))
->>> priv_key= openssl.evp.PKEY()
->>> priv_key.assign_RSA(openssl.rsa.RSA())
->>> key_data= key_file.read()
->>> key_file.close()
->>> import_privkey(key_data, priv_key.pkey.rsa)
-
->>> key_file= open(td('public/selma@test.yammer.net.gpub'))
->>> pub_key= openssl.evp.PKEY()
->>> pub_key.assign_RSA(openssl.rsa.RSA())
->>> pub_data= key_file.read()
->>> key_file.close()
->>> import_pubkey(key_data, pub_key.pkey.rsa)
-
->>> context= openssl.evp.MD_CTX()
->>> context.SignInit(openssl.evp.md5())
->>> context.SignUpdate('foobar')
->>> signature= context.SignFinal(priv_key)
->>> context= openssl.evp.MD_CTX()
->>> context.VerifyInit(openssl.evp.md5())
->>> context.VerifyUpdate('foobar')
->>> result = context.VerifyFinal(signature, pub_key)
->>> context= openssl.evp.CIPHER_CTX()
->>> iv, encrypted_keys= context.SealInit(openssl.evp.des_ede3_cbc(),
-...     [pub_key])
->>> result= context.SealUpdate('foo')
->>> encrypted_data= context.SealFinal()
->>> result= context.OpenInit(openssl.evp.des_ede3_cbc(),
-...     encrypted_keys[0], iv, priv_key)
->>> result= context.OpenUpdate(encrypted_data)
->>> result= context.OpenFinal()
->>> priv_key= None
->>> key_data= None
->>> key_file= None
->>> pub_data= None
->>> context= None
->>> signature= None
->>> result= None
->>> iv= None
->>> encrypted_keys= None
-
-"""
+#!/usr/bin/env python
+#
+# $Id: evp.py,v 1.2 2006/07/02 02:30:22 jtr Exp $
+#
 
 import opensslc
 try:
@@ -57,76 +11,108 @@ except ImportError:
 import rsa
 import wrap
 
+
 class MD(wrap.Wrapper):
-  def __init__(self, ptr= None):
-    wrap.Wrapper.__init__(self, ptr)
+	def __init__(self, ptr = None):
+		wrap.Wrapper.__init__(self, ptr)
+
+	
 
 class MD_CTX(wrap.Wrapper):
-  def __init__(self, ptr= None):
-    wrap.Wrapper.__init__(self, ptr, evpc.EVP_MD_CTX_new, evpc.EVP_MD_CTX_free)
+	def __init__(self, ptr = None):
+		wrap.Wrapper.__init__(self, ptr, evpc.EVP_MD_CTX_new, evpc.EVP_MD_CTX_free)
 
-  def SignInit(self, type):
-    evpc.EVP_SignInit(self.ptr, type.ptr)
 
-  def SignUpdate(self, signature_data):
-    evpc.EVP_SignUpdate(self.ptr, signature_data, len(signature_data))
+	def SignInit(self, type):
+		evpc.EVP_SignInit(self.ptr, type.ptr)
 
-  def SignFinal(self, key):
-    return evpc.python_EVP_SignFinal(self.ptr, key.ptr)
 
-  def VerifyInit(self, type):
-    evpc.EVP_VerifyInit(self.ptr, type.ptr)
+	def SignUpdate(self, signature_data):
+		evpc.EVP_SignUpdate(self.ptr, signature_data, len(signature_data))
 
-  def VerifyUpdate(self, signed_data):
-    evpc.EVP_VerifyUpdate(self.ptr, signed_data, len(signed_data))
 
-  def VerifyFinal(self, signature, key):
-    return evpc.EVP_VerifyFinal(self.ptr, signature, len(signature), key.ptr)
+	def SignFinal(self, key):
+		return evpc.python_EVP_SignFinal(self.ptr, key.ptr)
+
+
+	def VerifyInit(self, type):
+		evpc.EVP_VerifyInit(self.ptr, type.ptr)
+
+
+	def VerifyUpdate(self, signed_data):
+		evpc.EVP_VerifyUpdate(self.ptr, signed_data, len(signed_data))
+
+
+	def VerifyFinal(self, signature, key):
+		return evpc.EVP_VerifyFinal(self.ptr, signature, len(signature), key.ptr)
+
+
 
 class PKEY(wrap.Wrapper):
-  def __init__(self, ptr= None):
-    wrap.Wrapper.__init__(self, ptr, evpc.EVP_PKEY_new, evpc.EVP_PKEY_free,
-        {'pkey': (evpc.EVP_PKEY_pkey_get, None, PKEY_pkey)})
+	def __init__(self, ptr = None):
+		attr_dict = \
+		{
+			'pkey': (evpc.EVP_PKEY_pkey_get, None, PKEY_pkey),
+		}
+		wrap.Wrapper.__init__(self, ptr, evpc.EVP_PKEY_new, evpc.EVP_PKEY_free, attr_dict)
 
-  def assign_RSA(self, rsa_key): 
-    evpc.EVP_PKEY_assign_RSA(self.ptr, rsa_key.ptr)
-    rsa_key.free_ptr= 0
+
+	def assign_RSA(self, rsa_key): 
+		evpc.EVP_PKEY_assign_RSA(self.ptr, rsa_key.ptr)
+		rsa_key.free_ptr = 0
+
+
 
 class PKEY_pkey(wrap.Wrapper):
-  def __init__(self, ptr):
-    wrap.Wrapper.__init__(self, ptr, None, None, {'rsa':
-        (evpc.EVP_PKEY_pkey_rsa_get, evpc.EVP_PKEY_pkey_rsa_set, rsa.RSA)})
+	def __init__(self, ptr):
+		attr_dict = \
+		{
+			'rsa': (evpc.EVP_PKEY_pkey_rsa_get, evpc.EVP_PKEY_pkey_rsa_set, rsa.RSA),
+		}
+		wrap.Wrapper.__init__(self, ptr, None, None, attr_dict)
+
+
 
 class CIPHER(wrap.Wrapper):
-  def __init__(self, ptr= None):
-    wrap.Wrapper.__init__(self, ptr)
+	def __init__(self, ptr = None):
+		wrap.Wrapper.__init__(self, ptr)
+
+
 
 class CIPHER_CTX(wrap.Wrapper):
-  def __init__(self, ptr= None):
-    wrap.Wrapper.__init__(self, ptr, evpc.EVP_CIPHER_CTX_new,
-        evpc.EVP_CIPHER_CTX_free)
+	def __init__(self, ptr = None):
+		wrap.Wrapper.__init__(self, ptr, evpc.EVP_CIPHER_CTX_new, evpc.EVP_CIPHER_CTX_free)
 
-  def SealInit(self, type, public_keys):
-    return evpc.python_EVP_SealInit(self.ptr, type.ptr, public_keys)
 
-  def SealUpdate(self, unencrypted_data):
-    return evpc.python_EVP_SealUpdate(self.ptr, unencrypted_data)
+	def SealInit(self, type, public_keys):
+		return evpc.python_EVP_SealInit(self.ptr, type.ptr, public_keys)
 
-  def SealFinal(self):
-    return evpc.python_EVP_SealFinal(self.ptr)
 
-  def OpenInit(self, type, encrypted_key, iv, private_key):
-    return evpc.EVP_OpenInit(self.ptr, type.ptr, encrypted_key,
-        len(encrypted_key), iv, private_key.ptr)
+	def SealUpdate(self, unencrypted_data):
+		return evpc.python_EVP_SealUpdate(self.ptr, unencrypted_data)
 
-  def OpenUpdate(self, encrypted_data):
-    return evpc.python_EVP_OpenUpdate(self.ptr, encrypted_data)
 
-  def OpenFinal(self):
-    return evpc.python_EVP_OpenFinal(self.ptr)
+	def SealFinal(self):
+		return evpc.python_EVP_SealFinal(self.ptr)
+
+
+	def OpenInit(self, type, encrypted_key, iv, private_key):
+		return evpc.EVP_OpenInit(self.ptr, type.ptr, encrypted_key, len(encrypted_key), iv, private_key.ptr)
+
+
+	def OpenUpdate(self, encrypted_data):
+		return evpc.python_EVP_OpenUpdate(self.ptr, encrypted_data)
+
+
+	def OpenFinal(self):
+		return evpc.python_EVP_OpenFinal(self.ptr)
+
 
 def md5():
-  return MD(evpc.EVP_md5())
+	return MD(evpc.EVP_md5())
+
 
 def des_ede3_cbc():
-  return CIPHER(evpc.EVP_des_ede3_cbc())
+	return CIPHER(evpc.EVP_des_ede3_cbc())
+
+
